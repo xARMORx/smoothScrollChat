ae3c623bc3c16f666ebc17b7bfee5e7f1e54b07a
xARMORx <116925504+xARMORx@users.noreply.github.com>
2023-05-21 12:23:25 +0300

    addWndProc

--- smoothScrollChat/main.cpp
+++ smoothScrollChat/main.cpp
@@ -1,59 +1,21 @@
 #include "main.h"
-#include <chrono>
-#include <thread>
 
-void __fastcall CChat__PageUp(void* this_, void* EDX) {
-	std::thread([=]() {
-		if (!enable) {
-			enable = true;
-			int repeats = getChatScrollBarPos() - getPageSize();
-			if (repeats < 1) repeats = 1;
-			while (getChatScrollBarPos() != repeats) {
-				scrollChatScrollBar(-1);
-				setCursorScrollPosition();
-				std::this_thread::sleep_for(std::chrono::milliseconds(20));
-			}
-			enable = false;
+HRESULT __stdcall wndProc(HWND hWnd, UINT message, WPARAM wParam, unsigned int lParam) {
+	if (message == WM_KEYDOWN && !*(bool*)(0xB7CB49)) {
+		switch (wParam)
+		{
+		case VK_PRIOR:
+			CChat__PageUp();
+			break;
+		case VK_NEXT:
+			CChat__PageDown();
+			break;
+		default:
+			break;
 		}
-	}).detach();
-}
+	}
 
-void __fastcall CChat__PageDown(void* this_, void* EDX) {
-	std::thread([=]() {
-		if (!enable) {
-			enable = true;
-			int repeats{};
-			if (getChatScrollBarPos() != 100 - getPageSize()) {
-				if (getChatScrollBarPos() == 1)
-					repeats = getPageSize();
-				else if (getChatScrollBarPos() >= 100 - (getPageSize() * 2))
-					repeats = 100 - getPageSize();
-				else
-					repeats = getPageSize() + getChatScrollBarPos();
-				if (repeats > 100) repeats = 100;
-				while (getChatScrollBarPos() != repeats) {
-					scrollChatScrollBar(1);
-					setCursorScrollPosition();
-					std::this_thread::sleep_for(std::chrono::milliseconds(20));
-				}
-			}
-			enable = false;
-		}
-	}).detach();
-}
-
-void __fastcall CChat__ScrollToBottom(void* this_, void* EDX) {
-	std::thread([=]() {
-		if (!enable) {
-			enable = true;
-			while (getChatScrollBarPos() != 100 - getPageSize()) {
-				scrollChatScrollBar(1);
-				setCursorScrollPosition();
-				std::this_thread::sleep_for(std::chrono::milliseconds(5));
-			}
-			enable = false;
-		}
-	}).detach();
+	return wndProcHook->call<HWND, UINT, WPARAM, unsigned int>(hWnd, message, wParam, lParam);
 }
 
 void __cdecl CTimer__Update() {
@@ -63,54 +25,55 @@ void __cdecl CTimer__Update() {
 		{
 		case SAMP::sampVersion::R1:
 			CChat__ScrollToBottomAddr = { 0x637C0 };
-			CChat__PageDownAddr = { 0x63760 };
-			CChat__PageUpAddr = { 0x63700 };
 			setCursorPositionAddr = { 0x80D00 };
+			CChat__PageUpAddr = { 0x5D908 };
+			CChat__PageDownAddr = { 0x5D943 };
 			CChat = *(void**)(SAMP::GetSAMPHandle() + 0x21A0E4);
 			break;
 		case SAMP::sampVersion::R2:
 			CChat__ScrollToBottomAddr = { 0x63890 };
-			CChat__PageDownAddr = { 0x63830 };
-			CChat__PageUpAddr = { 0x637D0 };
 			setCursorPositionAddr = { 0x80DA0 };
+			CChat__PageUpAddr = { 0x5D9D8 };
+			CChat__PageDownAddr = { 0x5DA13 };
 			CChat = *(void**)(SAMP::GetSAMPHandle() + 0x21A0EC);
 			break;
 		case SAMP::sampVersion::R3:
 			CChat__ScrollToBottomAddr = { 0x66C10 };
-			CChat__PageDownAddr = { 0x66BB0 };
-			CChat__PageUpAddr = { 0x66B50 };
 			setCursorPositionAddr = { 0x84C10 };
+			CChat__PageUpAddr = { 0x60CA8 };
+			CChat__PageDownAddr = { 0x60CE3 };
 			CChat = *(void**)(SAMP::GetSAMPHandle() + 0x26E8C8);
 			break;
 		case SAMP::sampVersion::R4:
 			CChat__ScrollToBottomAddr = { 0x67350 };
-			CChat__PageDownAddr = { 0x672F0 };
-			CChat__PageUpAddr = { 0x67290 };
 			setCursorPositionAddr = { 0x85350 };
+			CChat__PageUpAddr = { 0x613D8 };
+			CChat__PageDownAddr = { 0x61413 };
 			CChat = *(void**)(SAMP::GetSAMPHandle() + 0x26E9F8);
 			break;
 		case SAMP::sampVersion::R5:
 			CChat__ScrollToBottomAddr = { 0x67390 };
-			CChat__PageDownAddr = { 0x67330 };
-			CChat__PageUpAddr = { 0x672D0 };
 			setCursorPositionAddr = { 0x85320 };
+			CChat__PageUpAddr = { 0x61418 };
+			CChat__PageDownAddr = { 0x61453 };
 			CChat = *(void**)(SAMP::GetSAMPHandle() + 0x26EB80);
 			break;
 		case SAMP::sampVersion::DL:
 			CChat__ScrollToBottomAddr = { 0x66E00 };
-			CChat__PageDownAddr = { 0x66DA0 };
-			CChat__PageUpAddr = { 0x66D40 };
 			setCursorPositionAddr = { 0x84DA0 };
+			CChat__PageUpAddr = { 0x60E98 };
+			CChat__PageDownAddr = { 0x60ED3 };
 			CChat = *(void**)(SAMP::GetSAMPHandle() + 0x2ACA10);
 			break;
 		default:
 			break;
 		}
 		pScrollbar = *(void**)((uint32_t)CChat + 0x11E);
-		CChat__PageUpHook = std::make_unique<memwrapper::memhook<CChat__PageUpPrototype>>(SAMP::GetSAMPHandle() + CChat__PageUpAddr, &CChat__PageUp);
-		CChat__PageUpHook->install();
-		CChat__PageDownHook = std::make_unique<memwrapper::memhook<CChat__PageDownPrototype>>(SAMP::GetSAMPHandle() + CChat__PageDownAddr, &CChat__PageDown);
-		CChat__PageDownHook->install();
+		memwrapper::fill_memory(SAMP::GetSAMPHandle() + CChat__PageUpAddr, 0x90, 11);
+		memwrapper::fill_memory(SAMP::GetSAMPHandle() + CChat__PageDownAddr, 0x90, 11);
+
+		wndProcHook = std::make_unique<memwrapper::memhook<wndProcPrototype>>(0x00747EB0, &wndProc);
+		wndProcHook->install();
 		CChat__ScrollToBottomHook = std::make_unique<memwrapper::memhook<CChat__ScrollToBottomPrototype>>(SAMP::GetSAMPHandle() + CChat__ScrollToBottomAddr, &CChat__ScrollToBottom);
 		CChat__ScrollToBottomHook->install();
 	}
@@ -124,7 +87,6 @@ AsiPlugin::AsiPlugin() {
 
 AsiPlugin::~AsiPlugin() {
 	CTimer__UpdateHook->remove();
-	CChat__PageUpHook->remove();
-	CChat__PageDownHook->remove();
 	CChat__ScrollToBottomHook->remove();
+	wndProcHook->remove();
 }
--- smoothScrollChat/main.h
+++ smoothScrollChat/main.h
@@ -5,15 +5,14 @@
 #include <memory>
 #include "memwrapper/memwrapper.h"
 #include "SAMP.hpp"
+#include <chrono>
+#include <thread>
 
 using CTimer__UpdatePrototype = void(__cdecl*)();
 std::unique_ptr<memwrapper::memhook<CTimer__UpdatePrototype>> CTimer__UpdateHook;
 
-using CChat__PageUpPrototype = void(__thiscall*)(void*);
-std::unique_ptr<memwrapper::memhook<CChat__PageUpPrototype>> CChat__PageUpHook;
-
-using CChat__PageDownPrototype = void(__thiscall*)(void*);
-std::unique_ptr<memwrapper::memhook<CChat__PageDownPrototype>> CChat__PageDownHook;
+using wndProcPrototype = HRESULT(__stdcall*)(HWND hWnd, UINT message, WPARAM wParam, unsigned int lParam);
+std::unique_ptr<memwrapper::memhook<wndProcPrototype>> wndProcHook;
 
 using CChat__ScrollToBottomPrototype = void(__thiscall*)(void*);
 std::unique_ptr<memwrapper::memhook<CChat__ScrollToBottomPrototype>> CChat__ScrollToBottomHook;
@@ -42,6 +41,60 @@ uint8_t getChatScrollBarPos() {
 
 void scrollChatScrollBar(uint8_t pos) {
 	*(uint8_t*)((uint32_t)pScrollbar + 0x8E) = getChatScrollBarPos() + pos;
+}
+
+void CChat__PageUp() {
+	std::thread([=]() {
+		if (!enable) {
+			enable = true;
+			int repeats = getChatScrollBarPos() - getPageSize();
+			if (repeats < 1) repeats = 1;
+			while (getChatScrollBarPos() != repeats) {
+				scrollChatScrollBar(-1);
+				setCursorScrollPosition();
+				std::this_thread::sleep_for(std::chrono::milliseconds(20));
+			}
+			enable = false;
+		}
+	}).detach();
+}
+
+void CChat__PageDown() {
+	std::thread([=]() {
+		if (!enable) {
+			enable = true;
+			int repeats{};
+			if (getChatScrollBarPos() != 100 - getPageSize()) {
+				if (getChatScrollBarPos() == 1)
+					repeats = getPageSize();
+				else if (getChatScrollBarPos() >= 100 - (getPageSize() * 2))
+					repeats = 100 - getPageSize();
+				else
+					repeats = getPageSize() + getChatScrollBarPos();
+				if (repeats > 100) repeats = 100;
+				while (getChatScrollBarPos() != repeats) {
+					scrollChatScrollBar(1);
+					setCursorScrollPosition();
+					std::this_thread::sleep_for(std::chrono::milliseconds(20));
+				}
+			}
+			enable = false;
+		}
+	}).detach();
+}
+
+void __fastcall CChat__ScrollToBottom(void* this_, void* EDX) {
+	std::thread([=]() {
+		if (!enable) {
+			enable = true;
+			while (getChatScrollBarPos() != 100 - getPageSize()) {
+				scrollChatScrollBar(1);
+				setCursorScrollPosition();
+				std::this_thread::sleep_for(std::chrono::milliseconds(5));
+			}
+			enable = false;
+		}
+		}).detach();
 }
 
 class AsiPlugin{
